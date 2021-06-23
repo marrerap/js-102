142da1ed48173e032695602e40463cee65719ea5
marrerap <andrew.marrero@outlook.com>
2021-06-23 15:59:10 -0500

    initial commit

---
+++ 1-print-numbers.js
@@ -0,0 +1,8 @@
+function printNumbers (num1, num2) {
+    for (count = num1; count <= num2; count ++) {
+        console.log(count)
+    }
+}
+
+printNumbers(1, 10)
+
---
+++ 2-print-square.js
@@ -0,0 +1,8 @@
+function printSquare (num1) {
+    for (count = 0; count <= num1; count ++) {
+        console.log("*")
+    }
+}
+
+printSquare(5)
+
